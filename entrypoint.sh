921659b643e2a88c78cfc64c799193ec38df4d5a
Bjørnøy, Simon <simon.bjornoy@gmail.com>
2025-04-23 22:20:05 +0200

    testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,6 @@
 
 set -eu
 
-# chown -R simon:1000 /home/simon/dev
-
-# # Switch to simon user and run the main command
-# exec gosu simon "$@"
-
 export XDG_CONFIG_HOME="/home/simon/.dotfiles"
 
 cd dev/ && tmux new-session
