67b3f06db23c80d179582917ce30c58118841158
Bjørnøy, Simon <simon.bjornoy@gmail.com>
2025-06-18 22:11:18 +0200

    feat: add ctrl-f as shortcut for dev in fish

--- env/.config/fish/config.fish
+++ env/.config/fish/config.fish
@@ -5,43 +5,27 @@
 export GOPATH="$HOME/go"
 export PATH="$PATH:$GOPATH/bin:/usr/local/go/bin:$HOME/.cargo/bin:$HOME/.local/scripts:/opt/nvim/bin:/home/linuxbrew/.linuxbrew/bin"
 
+bind \cf "dev"
+
 # DIRENV
 function __direnv_export_eval --on-event fish_prompt
-
     direnv export fish | source
-
     if test "$direnv_fish_mode" != disable_arrow
-
         function __direnv_cd_hook --on-variable PWD
-
             if test "$direnv_fish_mode" = eval_after_arrow
-
                 set -g __direnv_export_again 0
-
             else
-
                 direnv export fish | source
-
             end
-
         end
-
     end
-
 end
 
 function __direnv_export_eval_2 --on-event fish_preexec
-
     if set -q __direnv_export_again
-
         set -e __direnv_export_again
-
         direnv export fish | source
-
         echo
-
     end
-
     functions --erase __direnv_cd_hook
-
 end
